ecc64b484f215eb40236ebf69f35bb94c343c606
Nathan <malbun08@mailfence.com>
2025-08-13 19:54:56 +0200

    Simplified return

--- src/utils/StationUtils.js
+++ src/utils/StationUtils.js
@@ -1,18 +1,16 @@
 export const getStationDetails = async (station) => {
     const didokRequestURL = `https://data.sbb.ch/api/explore/v2.1/catalog/datasets/dienststellen-gemass-opentransportdataswiss/records?select=designationofficial, number&limit=20&refine=designationofficial:${station}`
-    const didokResult = await fetch(didokRequestURL, {
-        method: "GET"
+  return await fetch(didokRequestURL, {
+      method: "GET"
     })
         .then(res => res.text())
         .then(json => {
-            return JSON.parse(json);
+          return JSON.parse(json);
         })
         .then(jsObject => {
-            const result = {};
-            result.didok = jsObject["results"][0]["number"];
-            result.stationName = jsObject["results"][0]["designationofficial"];
-            return result;
+          const result = {};
+          result.didok = jsObject["results"][0]["number"];
+          result.stationName = jsObject["results"][0]["designationofficial"];
+          return result;
         });
-
-    return didokResult;
 }
