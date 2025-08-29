4a4c02f1033f581af8f575f2d831ed197473f26f
Malbu <malbun08@mailfence.com>
2025-08-29 07:41:43 +0200

    changed router config t base url /Fahrplan/

--- src/router.ts
+++ src/router.ts
@@ -4,30 +4,29 @@ import Departure from "./components/ArrivalDeparture/Departure.vue";
 import ArrivalDepartureOverview from "./components/ArrivalDeparture/ArrivalDepartureOverview.vue";
 import Trip from "./components/Trip/Trip.vue";
 
-const routes = [
-  {
-    path: "/arrdep",
-    component: ArrivalDepartureOverview,
-    children: [
-      {
-        path: "/arr",
-        component: Arrival,
-      },
-      {
-        path: "/dep",
-        component: Departure,
-      },
-    ],
-  },
-  {
-    path: "/trip",
-    component: Trip,
-  },
-];
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes,
+  history: createWebHistory("/Fahrplan/"),
+  routes: [
+    {
+      path: "/arrdep",
+      component: ArrivalDepartureOverview,
+      children: [
+        {
+          path: "/arr",
+          component: Arrival,
+        },
+        {
+          path: "/dep",
+          component: Departure,
+        },
+      ],
+    },
+    {
+      path: "/trip",
+      component: Trip,
+    },
+  ],
 });
 
 export default router;
