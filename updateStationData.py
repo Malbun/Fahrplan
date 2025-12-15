792f6ee977d73df8b7f10818706903bcc70d64f1
Malbun <120336653+Malbun@users.noreply.github.com>
2025-12-15 22:58:29 +0100

    Added print statements for logging

--- updateStationData.py
+++ updateStationData.py
@@ -4,13 +4,19 @@ import requests
 data = None
 newData = []
 
+print("Start fetching data...")
+
 response = requests.get("https://data.sbb.ch/api/explore/v2.1/catalog/datasets/dienststellen-gemass-opentransportdataswiss/exports/json")
 if response.status_code == 200:
+    print("Data fetched")
+    
+    print("Start processing data...")
     data = response.json()
 
 else:
     raise IOError("Get bad response code from api: " + str(response.status_code))
 
+print("Processing data...")
 for item in data:
     if item["meansoftransport"] is not None and item["meansoftransport"] != "UNKNOWN":
         abbreviation = item["abbreviation"]
@@ -21,8 +27,12 @@ for item in data:
         }
         newData.append(itemData)
 
+print("Finished processing data")
+
+print("Start saving processed data...")
 outputFile = open("src/data/stationData.json", "w")
 outputFile.write(json.dumps(newData))
 outputFile.close()
 
-
+print("Finished saving processed data")
+print("Job completed")
